874ffa22ca118aaaf71f1e612d5e8c55acb147c0
gourab8389 <deyg6988@gmail.com>
2025-03-05 00:22:38 +0530

    feat: integrate Uploadthing for file uploads with custom router and components

---
+++ src/app/api/uploadthing.ts
@@ -0,0 +1,10 @@
+import { ourFileRouter } from "@/server/uploadthing";
+import { createRouteHandler } from "uploadthing/next-legacy";
+
+
+export default createRouteHandler({
+  router: ourFileRouter,
+
+  // Apply an (optional) custom config:
+  // config: { ... },
+});
