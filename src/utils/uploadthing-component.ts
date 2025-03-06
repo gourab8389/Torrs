e0987c6128a70d40644c56a3c2bc3958d14c47a2
gourab8389 <deyg6988@gmail.com>
2025-03-06 23:43:59 +0530

    feat: add PhoneInput component with country selection and custom styling

--- src/utils/uploadthing-component.ts
+++ src/utils/uploadthing-component.ts
@@ -1,10 +1,9 @@
 import { OurFileRouter } from "@/server/uploadthing";
 import {
-        generateUploadButton,
-        generateUploadDropzone,
-      } from "@uploadthing/react";
-      
-      
-      export const UploadButton = generateUploadButton<OurFileRouter>();
-      export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
-      
+  generateUploadButton,
+  generateUploadDropzone,
+} from "@uploadthing/react";
+
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
